de5a42e6ff461ef561ccd585489de36b1d361b2d
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2024-01-09 13:39:45 +0530

    backend-task-3

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(first_name: "Tushar", last_name: "Jaitwal", email: "tushar@test.com", phone: "9897654654", address: "Indore")
+User.create(first_name: "Sonu", last_name: "Yadav", email: "sonu@test.com", phone: "9893654654", address: "Indore")
+User.create(first_name: "Monu", last_name: "Soni", email: "monushar@test.com", phone: "989765r654", address: "Sanwer")
+User.create(first_name: "Raj", last_name: "verma", email: "raj@test.com", phone: "9897r54654", address: "Indore")
